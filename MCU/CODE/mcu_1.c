d0bc093e79de9ece1db173d97645663a1740e428
zhengqi <1482236259@qq.com>
2018-05-28 14:06:25 +0800

    finish all

--- MCU/CODE/mcu_1.c
+++ MCU/CODE/mcu_1.c
@@ -4,25 +4,24 @@
 #include <math.h>
 typedef unsigned int u16;	  
 typedef unsigned char u8;
-
-u8 cmd[2]={0,0};
-u16 num1=8,num2,num3;
-u8 Dis[4]={0,0,0,0};
+u8 cmd[2]={'0','0'};
+u16 num1=0,num2=0,num3=0;
+u8 num1_1,num1_2,num1_3,num1_4,num2_1,num2_2,num2_3,num2_4,num3_1,num3_2,num3_3,num3_4;
+u8 code smgduan[10]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f};
+u8 hour=12,min=0,sec=0;
 sbit LSA=P2^2;
 sbit LSB=P2^3;
 sbit LSC=P2^4;
 sbit LSD=P2^5;
-
 void delay(u16 i)
 {
 	while(i--);	
 }
 
-void DigDisplay(u16 bad)
+void DigDisplay(u16 flag)
 {
 	u8 i;
-	u8 code smgduan[10]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f};
-	if(bad==1){
+	if(flag==1){
 		for(i=0;i<3;i++)
 		{
 			switch(i)
@@ -38,8 +37,7 @@ void DigDisplay(u16 bad)
 			P0=0x00;
 		}
 	}
-	else{
-		u8 num1_1,num1_2,num1_3,num1_4,num2_1,num2_2,num2_3,num2_4,num3_1,num3_2,num3_3,num3_4;
+	else if(flag==0){
 		num1_4=num1%10;
 		num1_3=((num1-num1_4)/10)%10;
 		num1_2=((num1-num1_3*10-num1_4)/100)%10;
@@ -52,7 +50,7 @@ void DigDisplay(u16 bad)
 		num3_3=((num3-num3_4)/10)%10;
 		num3_2=((num3-num3_3*10-num3_4)/100)%10;
 		num3_1=((num3-num3_2*100-num3_3*10-num3_4)/1000)%10;
-		for(i=0;i<12;i++)
+		for(i=0;i<8;i++)
 		{
 			switch(i)	 //位选，选择点亮的数码管，
 			{
@@ -81,12 +79,44 @@ void DigDisplay(u16 bad)
 				case(11):
 					LSA=1;LSB=1;LSC=0;LSD=1;P0=smgduan[num3_4]; break;//显示第2位
 			}
-			//LSA=0;LSB=0;LSC=0;LSD=0;
-			//P0=1;
 			delay(100); //间隔一段时间扫描	
 			P0=0x00;//消隐
 		}		
 	}
+	else if(flag==2){
+		for(i=0;i<8;i++)
+		{
+			switch(i)	 //位选，选择点亮的数码管，
+			{
+				case(0):
+					LSA=0;LSB=0;LSC=0;LSD=0;P0=smgduan[hour/10]; break;//显示第0位
+				case(1):
+					LSA=1;LSB=0;LSC=0;LSD=0;P0=smgduan[hour%10]; break;//显示第1位
+				case(2):
+					LSA=0;LSB=1;LSC=0;LSD=0;P0=0x40; break;//显示第2位
+				case(3):
+					LSA=1;LSB=1;LSC=0;LSD=0;P0=smgduan[min/10]; break;//显示第3位
+				case(4):
+					LSA=0;LSB=0;LSC=1;LSD=0;P0=smgduan[min%10]; break;//显示第4位
+				case(5):
+					LSA=1;LSB=0;LSC=1;LSD=0;P0=0x40; break;//显示第5位	
+				case(6):
+					LSA=0;LSB=1;LSC=1;LSD=0;P0=smgduan[sec/10]; break;
+				case(7):
+					LSA=1;LSB=1;LSC=1;LSD=0;P0=smgduan[sec%10]; break;
+				case(8):
+					LSA=0;LSB=0;LSC=0;LSD=1;P0=smgduan[num3_1]; break;
+				case(9):
+					LSA=1;LSB=0;LSC=0;LSD=1;P0=smgduan[num3_2]; break;//显示第0位
+				case(10):
+					LSA=0;LSB=1;LSC=0;LSD=1;P0=smgduan[num3_3]; break;//显示第1位
+				case(11):
+					LSA=1;LSB=1;LSC=0;LSD=1;P0=smgduan[num3_4]; break;//显示第2位
+			}
+			delay(100); //间隔一段时间扫描	
+			P0=0x00;//消隐
+		}
+	}		
 }
 
 void UsartInit()
@@ -100,6 +130,16 @@ void UsartInit()
 	EA=1;						
 	TR1=1;  	
 }
+
+void TimerInit()
+{
+	TMOD=0X21;
+	TH0=0X3C;
+	TL0=0XB0;
+	ET0=1;
+	TR0=1;
+}
+	
 
 u16 isprime(u16 n)
 {
@@ -135,54 +175,74 @@ void main()
 	P2=0xff;
 	UsartInit();
 	while(1){
-		if(num1%2==1){
-			DigDisplay(1);
-		}
-		else{
-			num2=fenjie(num1);
-			num3=num1-num2;
-				for(k=0;k<60;k++){
-					DigDisplay(0);
-				}
-				if(cmd[0]=='S' && cmd[1]=='T'){
-					num1+=2;
-				}
+		if(cmd[0]=='T' && cmd[1]=='M'){
+			TimerInit();
+			for(k=0;k<60;k++){
+				DigDisplay(2);
 			}
 		}
-	
+		if(cmd[0]=='G' && cmd[1]=='O'){
+			if(num1%2==1){
+				DigDisplay(1);
+			}
+			else if(5<num1<10000){
+				num2=fenjie(num1);
+				num3=num1-num2;
+					while(1){
+						DigDisplay(0);
+					}
+				}
+			}
+		if(cmd[0]=='S' && cmd[1]=='T'){
+			while(1){
+				num2=fenjie(num1);
+				num3=num1-num2;
+					for(k=0;k<150;k++){
+						DigDisplay(0);
+					}
+				num1+=2;
+			}
+		}
+	}
 }
 
 void Usart() interrupt 4
 {
 	u8 receiveData;
-	cmd[0]='S';
-	cmd[1]='T';
 	receiveData=SBUF;
 	RI = 0;
 	SBUF=receiveData;
 	while(!TI);			 
 	TI=0;
-	
-	/*if(a==0){
-		a=SBUF;
+	if(cmd[0]=='0'){
+		cmd[0]=SBUF;
 	}
-	else if(b==0){
-		b=SBUF;
+	else if(cmd[1]=='0'){
+		cmd[1]=SBUF;
 	}
-	else if(Dis[0]==0){
-		Dis[0]=SBUF;
-		//num1=10*num1+Dis[0]-'0';
+	else if(cmd[1]!='0'){
+		num1=10*num1+SBUF-'0';
 	}
-	else if(Dis[1]==0){
-		Dis[1]=SBUF;
-		//num1=10*num1+SBUF-'0';
+}
+
+void Timer() interrupt 2
+{
+	static u8 times=0;
+	times+=1;
+	//delay(1600);
+	if(times==12){
+		sec+=1;
+		times=0;
 	}
-	else if(Dis[2]==0){
-		Dis[2]=SBUF;
-		//num1=10*num1+SBUF-'0';
+	if(sec==60){
+		min+=1;
+		sec=0;
 	}
-	else if(Dis[3]==0){
-		Dis[3]=SBUF;
-		//num1=10*num1+SBUF-'0';
-	}*/
+	if(min==60){
+		hour+=1;
+		min=0;
+	}
+	if(hour==24){
+		hour=0;
+	}
 }
